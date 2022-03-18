9556c6dbaa8015045459a97111e018a76ce26f04
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-03-18 23:26:04 +0100

    🚧 work in progress

--- graph.go
+++ graph.go
@@ -12,20 +12,25 @@ type Pair[U, V any] struct {
 	Second V
 }
 
-type Graph_[NodeType comparable, LabelType any] struct {
-	Nodes  map[NodeType]bool
-	Edges  map[Pair[NodeType, NodeType]]bool
-	Labels map[Pair[NodeType, NodeType]]LabelType // would make more sense to have a single map
+type Edge_[N comparable] struct {
+	Source N
+	Target N
+}
+
+type Graph_[N comparable, L any] struct {
+	Nodes  map[N]bool
+	Edges  map[Edge_[N]]bool
+	Labels map[Edge_[N]]L // would make more sense to have a single map
 	// (less errors by construction, optimisation, etc.).
 	// BUT there the conceptual design is clearer AND labels can be nil
 	// (we don't "pay" for labels when there is not need).
 }
 
-func New_[NodeType comparable, LabelType any]() *Graph_[NodeType, LabelType] {
-	graph := new(Graph_[NodeType, LabelType])
-	graph.Nodes = map[NodeType]bool{}
-	graph.Edges = map[Pair[NodeType, NodeType]]bool{}
-	graph.Labels = map[Pair[NodeType, NodeType]]LabelType{}
+func New_[N comparable, L any]() *Graph_[N, L] {
+	graph := new(Graph_[N, L])
+	graph.Nodes = map[N]bool{}
+	graph.Edges = map[Edge_[N]]bool{}
+	graph.Labels = map[Edge_[N]]L{}
 	return graph
 }
 
@@ -35,7 +40,7 @@ func (graph Graph_[N, L]) AddNode(nodes ...N) {
 	}
 }
 
-func (graph Graph_[N, L]) AddEdge(edges ...Pair[N, N]) {
+func (graph Graph_[N, L]) AddEdge(edges ...Edge_[N]) {
 	for _, edge := range edges {
 		graph.Edges[edge] = true
 	}
@@ -45,23 +50,63 @@ func (graph Graph_[N, L]) String() string {
 	return fmt.Sprintf("nodes: %v\nedges: %v\nlabels: %v", graph.Nodes, graph.Edges, graph.Labels)
 }
 
-func (graph *Graph_[N, L]) PathTo(source, destination N) ([]N, error) {
-	var path []N
-	message := fmt.Sprintf("no path from %v to %v", source, destination)
-	err := errors.New(message)
-	return path, err
+func (graph *Graph_[N, L]) Neighbors(node N) map[N]bool {
+	neighbors := map[N]bool{}
+	for edge, _ := range graph.Edges {
+		if edge.Source == node {
+			neighbors[edge.Target] = true
+		}
+	}
+	return neighbors
+}
+
+func pop[K comparable](m map[K]bool) (K, error) {
+	var k K
+	if len(m) == 0 {
+		return k, errors.New(fmt.Sprintf("empty map %v.\n", m))
+	}
+	for k = range m {
+		break
+	}
+	delete(m, k)
+	return k, nil
+}
+
+func (graph *Graph_[N, L]) PathTo(source, target N) []N {
+	pathMap := map[N]([]N){source: []N{source}} // node -> path
+	todo := map[N]bool{source: true}            // set of nodes
+	done := map[N]bool{}                        // set of nodes
+	for {
+		node, err := pop(todo)
+		if err != nil {
+			break
+		}
+		path := pathMap[node]
+		for neighbor := range graph.Neighbors(node) {
+			if done[neighbor] {
+				continue
+			}
+			newPath := make([]N, len(path), len(path)+1)
+			copy(newPath, path)
+			newPath = append(newPath, neighbor)
+			if neighbor == target {
+				return newPath
+			}
+			pathMap[neighbor] = newPath
+			todo[neighbor] = true
+		}
+		done[node] = true
+	}
+	return nil
 }
 
 // -----------------------------------------------------------------------------
 
-type Node = int
-type Edge = Pair[Node, Node]
-type Label = string
+type Node = [2]int
+type Edge = Edge_[Node]
+type Label = float64
 type Graph = Graph_[Node, Label]
 
-// func New() *Graph { // no way to do this without an extra function call?
-// 	return New_[Node, Label]()
-// }
 var New func() *Graph = New_[Node, Label]
 
 func main() {
@@ -69,12 +114,10 @@ func main() {
 	// fmt.Printf("%T\n", graph)
 	// fmt.Printf("%#v %#v %#v\n", graph.Nodes, graph.Edges, graph.Labels)
 	//fmt.Println(graph.Nodes == nil)
-	graph.AddNode(1, 2, 3)
-	graph.AddEdge(Edge{1, 2}, Edge{2, 3})
+	graph.AddNode(Node{0, 0}, Node{1, 0}, Node{1, 1})
+	graph.AddEdge(Edge{Node{0, 0}, Node{0, 1}}, Edge{Node{0, 1}, Node{1, 1}})
 	fmt.Println(graph)
-	_, err := graph.PathTo(1, 2)
-	if err != nil {
-		panic(err)
-	}
+	path := graph.PathTo(Node{0, 0}, Node{1, 1})
+	fmt.Println("path:", path)
 
 }
